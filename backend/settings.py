db59278b0261615d05b4c62fd2f0b0311e6d9d0b
badr-jr <badrmohamed902@gmail.com>
2021-04-09 03:47:57 +0200

    update heroku configs

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(wk%nub5yxucbvitp=!9@bqc#)cxd37_&9brc7u3(rx&8q7^se'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
